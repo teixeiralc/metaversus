2e44ff5274891ac4654930c493db9b5b04d9f727
Lucas Teixeira <lucascteixeira0@gmail.com>
2023-01-19 10:20:21 -0300

    add whatsNew section

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,6 +1,12 @@
-const NewFeatures = () => (
-  <div>
-    New Features
+import styles from '../styles';
+
+const NewFeatures = ({ imgUrl, title, subtitle }) => (
+  <div className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]">
+    <div className={`${styles.flexCenter} w-[70px] h-[70px] rounded-3xl bg-[#323f5d]`}>
+      <img src={imgUrl} alt="" className="w-1/2 h-1/2 object-contain" />
+    </div>
+    <h3 className="mt-6 font-bold text-2xl text-white">{title}</h3>
+    <p className="flex-1 mt-4 text-lg text-[#b0b0b0]">{subtitle}</p>
   </div>
 );
 
--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,8 +1,33 @@
 'use client';
 
+import { motion as m } from 'framer-motion';
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+import { NewFeatures, TitleText, TypingText } from '../components';
+import { newFeatures } from '../constants';
+import styles from '../styles';
+
 const WhatsNew = () => (
-  <section>
-    What's new section
+  <section className={`${styles.paddings} relative z-10`}>
+    <m.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: 'false', amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+    >
+      <m.div variants={fadeIn('right', 'tween', 0.2, 1)} className="flex-[0.75] flex justify-center flex-col">
+        <TypingText title="| What's new" />
+        <TitleText title="What's new about Metaversus?" />
+        <div className="mt-12 flex flex-wrap justify-between gap-6">
+          {newFeatures.map((feat) => (
+            <NewFeatures key={feat.title} {...feat} />
+          ))}
+        </div>
+      </m.div>
+      <m.div variants={planetVariants('right')} className={`${styles.flexCenter} flex-1`}>
+        <img src="/whats-new.png" alt="What's new" className="w-11/12 h-11/12 object-contain" />
+      </m.div>
+    </m.div>
   </section>
 );
 
